2fcbce5c161e3bfd5a2274414eb1e64e74cd8c60
Syed Hamza <syedhamzatahir1001@gmail.com>
2023-06-11 04:03:17 -0700

    Layout changed of About section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,152 +1,343 @@
-import React,{useState} from 'react'
-import headshot from '../assets/images/headshot.jpg'
-import headshotWhite from '../assets/images/headshot-white.jpg'
-import faisal from '../assets/images/faisal-mosque.png'
-import { useSelector } from 'react-redux';
-import codesticker from '../assets/images/code.jpg';
-import guitar from '../assets/images/guitar.png';
-import game from '../assets/images/game.gif';
-import brain from '../assets/images/brain.webp';
-import skills from '../assets/images/skills.webp';
-import eye from '../assets/images/education.gif';
-import signature from '../assets/images/signature.png';
-import spotify from '../assets/images/spotify.png';
-import heart from '../assets/images/heart.gif'
-import {motion} from 'framer-motion';
- 
+import React, { useState, useRef } from "react";
+import headshot from "../assets/images/headshot.jpg";
+import headshotWhite from "../assets/images/headshot-white.jpg";
+import faisal from "../assets/images/faisal-mosque.png";
+import { useSelector } from "react-redux";
+import signature from "../assets/images/signature.png";
+import computer from "../assets/images/computer.jpg";
+import headphones from "../assets/images/headphones.jpg";
+import briefcase from "../assets/3D/Briefcase.png";
+import certificate from "../assets/3D/Certificate.png";
+import { AiOutlineArrowRight } from "react-icons/ai";
+import Counter from "./Counter";
+import guitar from "../assets/3D/Ukulele.png";
+import music from "../assets/3D/music.png";
+import coffee from "../assets/3D/coffee.png";
+import { spring, useVariants } from "./config";
+import { motion } from "framer-motion";
+import { Player, Controls } from "@lottiefiles/react-lottie-player";
 
 export default function About() {
+  const dark = useSelector((state) => state.switch.value);
+  const [showSkills, setShowSkills] = useState(false);
+  const [cursorImage, setCursorImage] = useState(1);
+  const [cursorVariant, setCursorVariant] = useState("default");
+  const ref = useRef(null);
+  const hoverOnImage = cursorImage === 2 ? briefcase :  certificate
 
-  
-  const dark = useSelector((state) => state.switch.value)
-  const [showSkills, setShowSkills] = useState(false)
- 
+  const variants = useVariants(ref);
+
+  function EducationEnter() {
+    setCursorImage(1);
+    setCursorVariant("education");
+  }
+
+  function Leave() {
+    setCursorVariant("default");
+  }
+
+  function ExperienceEnter() {
+    setCursorImage(2);
+    setCursorVariant("experience");
+  }
+
+  function CircleLink() {
+    return (
+      <div className="w-14 h-14 border border-black flex flex-row items-center justify-center rounded-full bg-[#FFC633]">
+        <AiOutlineArrowRight className="w-8 h-8 -rotate-45" />
+      </div>
+    );
+  }
+
+  function Headings({ name, isShowLink, reverse }) {
+    const text_color = dark ? "text-gray-300" : "text-gray-500";
+    const direction = reverse ? "flex-row" : "flex-row";
+    return (
+      <div className={`flex ${direction} items-center justify-between mx-2`}>
+        <h2 className={`font-satoshiMd ${text_color} text-3xl`}>{name}</h2>
+        {isShowLink && <CircleLink />}
+      </div>
+    );
+  }
+
+  function ExperienceBrief({ position, company, date }) {
+    return (
+      <>
+        <p
+          className={`mt-2 ${
+            dark ? "text-white" : "text-black"
+          } font-satoshiB text-lg mx-2`}
+        >
+          {position}
+        </p>
+        <p
+          className={`${
+            dark ? "text-white" : "text-black"
+          } font-satoshiRg text-lg mx-2`}
+        >
+          {company}
+        </p>
+        <p
+          className={`${
+            dark ? "text-white" : "text-black"
+          } font-satoshiMd text-xs mx-2`}
+        >
+          {date}
+        </p>
+      </>
+    );
+  }
+
   return (
-    <div className={`relative border  h-screen ${dark ? "border-black" : "border-white"} `}>
-      <div className='absolute -top-20 m-0'>
-<h1 className={`font-chillaxSemi ${dark ? "border-black" : "border-white"}  ${dark && "text-primary-800"}  project-heading`}>Quick Tour</h1>
-</div>
-<div className='flex items-center justify-center mt-16'>
+    <div className="App">
+      <div>
+        <div className="container" ref={ref}>
+          <motion.div
+            variants={variants}
+            className="circle"
+            animate={cursorVariant}
+            transition={spring}
+          >
+            <div className="hover-main w-32 h-32">
+                <img src={hoverOnImage} width="50" height="50" />
+                <svg id="rotatingText" width="120" height="120">
+                  <defs>
+                    <path
+                      id="hover-circle"
+                      d="M 60, 60
+                 m -45, 0
+                 a 45, 45 0 1, 0 90, 0
+                 a 45,45 0 1, 0 -90, 0
+                 "
+                    ></path>
+                  </defs>
+                  <text width="400">
+                    <textPath
+                      alignmentBaseline="top"
+                      xlinkHref="#hover-circle"
+                      className=" font-satoshiRg hover-text fill-white"
+                    >
+                      · See more · See more · See more 
+                    </textPath>
+                  </text>
+                </svg>
+              </div>
+            {/* <div className="flex flex-col items-center">
+              <img src={eye}  className="w-36"/>
+            <span className="text-3xl font-satoshiRg ">{cursorText}</span>
+            </div> */}
+          </motion.div>
 
-<div className="parent gap-4 w-11/12 ">
-      
-<div className="div1 z-10 flex flex-col items-center justify-center relative place-self-center">
-  {/* <img src={eye} className='w-36 rounded-full'/> */}
-  <div>
-  <img src={dark ? headshotWhite: headshot} className='rounded-full'/>
-  <img src={signature} className='w-full absolute bottom-[20%]'/>
-  </div>
-  
-</div>
-<div className="div2 z-10 place-self-center">
+          <div className="h-screen grid grid-rows-4 grid-cols-3">
+            <div className="">
+              <Headings name="Biography" />
+              <p
+                className={`mt-2 ${
+                  dark ? "text-white" : "text-black"
+                } font-satoshiRg mx-2 text-lg `}
+              >
+                I'm Syed, a passionate and ambitious individual based in
+                Pakistan. My deep-rooted passion for AI drives me to excel in
+                creating innovative solutions.
+              </p>
+            </div>
+            <div className=" row-span-4  place-self-center relative">
+              <div className="card-group">
+                <div className="little-card card">
+                  <img src={guitar} className="rounded-full" />
+                </div>
+                <div className="little-card card">
+                  <img src={music} className="rounded-full" />
+                </div>
+                <div className="little-card card">
+                  <img src={coffee} className="rounded-full" />
+                </div>
+                <div className="flex flex-col items-center justify-center">
+                  <img
+                    src={dark ? headshotWhite : headshot}
+                    className="w-4/5 rounded-full"
+                  />
+                  <img
+                    src={signature}
+                    className="w-3/4 absolute bottom-[10%]"
+                  />
+                  <div className="absolute w-4/5 h-full rounded-full picture-outline"></div>
+                </div>
+              </div>
+            </div>
+            <div className="">
+              <Headings name="Soft Skills" reverse={true} />
+              <p
+                className={`mt-2 ${
+                  dark ? "text-white" : "text-black"
+                } font-satoshiB text-right text-3xl mx-2`}
+              >
+                Team Player · Autodidactic · Adaptable · Creative
+              </p>
+            </div>
 
-<motion.div
-      className="rounded-3xl p-3 w-full h-full flex flex-col items-center justify-center info-bg"
-      whileHover={{ scale: 1.02 }}
-      transition={{ type: "spring", stiffness: 400, damping: 10 }}
-  >
-    <p  className="text-3xl p-1 mt-1 font-generalSansMd text-white text-center">Specialized in AI, web & mobile application development</p>
-  <div className='flex flex-row'>
-  <motion.div  className='cursor-default rounded-full text-center bg-black text-white bottom-0 left-16 px-4 py-1 m-2'>
-    <p className='font-generalSansMd  text-sm'>Web Developer</p>
-    <p className='text-xs font-generalSansRg text-gray-300'>6 months</p>
-  </motion.div>
-    <motion.div className='cursor-default  rounded-full text-center bg-white bottom-0 left-16 px-4 py-1 m-2'>
-    <p className='font-generalSansMd text-sm'>AI Engineer</p>
-    <p className='text-xs font-generalSansRg  text-gray-600'>1+ years</p>
-  </motion.div>
-  </div>
-  <p className='font-generalSansMd text-white text-sm mt-2'>See more</p>
-
-  </motion.div>
-
-  
-</div>
-<div className="div3 z-10 place-self-center">
-<div>
-    <div className='main'>
-    <img src={faisal} width="65" height="65"/>
-    <svg id="rotatingText"  width="150" height="150">
-      <defs>
-        <path id="circle" d="M 75, 75
-                m -56.25, 0
-                a 56.25, 56.25 0 1, 0 112.5, 0
-                a 56.25, 56.25 0 1, 0 -112.5, 0
-                ">
-        </path>
-      </defs>
-      <text width="400">
-        <textPath alignmentBaseline="top" xlinkHref="#circle" className="text font-generalSansRg">
-        · Lives in Pakistan · Lives in Pakistan   
-        </textPath>
-      </text>
-    </svg> 
-    </div>
-  </div>
-</div>
-<div className="div4 z-10 place-self-center">
-<motion.div
-      className="rounded-3xl p-3 w-full h-full text-center flex items-center justify-center text-white bg-educate text-3xl font-generalSansMd"
-      whileHover={{ scale: 1.05 }}
-      transition={{ type: "spring", stiffness: 400, damping: 10 }} 
-  >
-    <div>
-    <p className='p-1 mt-1'>Bachelor's in Computer Science</p>
-    <motion.div className=' flex items-center justify-center '>
-    <motion.div  className='cursor-default rounded-full text-center bg-black text-white py-1 px-4  m-2'>
-    <p className='font-generalSansMd  text-sm'>FAST NUCES</p>
-    <p className='text-xs font-generalSansRg text-gray-300'>Islamabad</p>
-  </motion.div>
-    <div className='cursor-default rounded-full text-center bg-white p-3 m-2'>
-    <div className='flex items-center'><p className='text-black font-generalSansMd  text-sm'>GPA <span className='text-gray-300'><strong className='text-primary-500 '>  3.05 </strong> / 4</span></p></div>
-    </div>
-    
-  </motion.div>
-    <p className='text-sm mt-2'>See more</p>
-    </div> 
-
-  </motion.div> 
-</div>
-<div className='div5 z-10 w-full h-full place-self-center'>
-<motion.div onMouseOver={() => setShowSkills(true)} onMouseLeave={() => setShowSkills(false)} className={`bg-skills p-2 h-full   rounded-full flex flex-col items-center justify-evenly text-center text-xl font-generalSansMd text-white`} whileHover={{ scale: 1.1 }} transition={{ type: "spring", stiffness: 400, damping: 10 }}>
-      <div className={`${showSkills ? "hidden":"block"} shadow-sm p-2 m-1 bg-white rounded-2xl text-[#FFC145] cursor-default`}>
-        Soft Skills
+            <div className="">
+              <Headings name="Services" />
+              <p
+                className={`mt-2 ${
+                  dark ? "text-white" : "text-black"
+                } font-satoshiRg text-lg mx-2`}
+              >
+                We excel in <strong>Web</strong>, <strong>AI</strong> and{" "}
+                <strong>App</strong> development, delivering captivating
+                solutions with innovative tech, seamless integration, and
+                stunning designs that leave a lasting impression.
+              </p>
+            </div>
+            <div className="">
+              <Headings name="Projects Done" reverse={true} />
+              <div
+                className={`mt-2 ${
+                  dark ? "text-white" : "text-black"
+                } font-satoshiB text-right text-3xl mx-2`}
+              >
+                <Counter value={25} />+ projects
+              </div>
+            </div>
+            <div className="">
+              <div
+                className={`flex flex-row items-center justify-between mx-2`}
+              >
+                <h2
+                  className={`font-satoshiMd ${
+                    dark ? "text-gray-300" : "text-gray-500"
+                  }  text-3xl`}
+                >
+                  Education
+                </h2>
+                <div
+                  onMouseEnter={EducationEnter}
+                  onMouseLeave={Leave}
+                  className="w-14 h-14 border border-black flex flex-row items-center justify-center rounded-full bg-[#FFC633]"
+                >
+                  <AiOutlineArrowRight className="w-8 h-8 -rotate-45" />
+                </div>
+              </div>
+              <div className="flex flex-row items-center justify-between">
+                <div>
+                  <p
+                    className={`mt-2 ${
+                      dark ? "text-white" : "text-black"
+                    } font-satoshiRg text-lg mx-2`}
+                  >
+                    Bachelor of Science, CS
+                  </p>
+                  <p className={`text-primary-500 font-satoshiMd text-lg mx-2`}>
+                    FAST NUCES
+                  </p>
+                  <p
+                    className={`${
+                      dark ? "text-white" : "text-black"
+                    } font-satoshiMd text-xs mx-2`}
+                  >
+                    09/2018 - 01/2023
+                  </p>
+                </div>
+                <div className="text-center border-l">
+                  <p
+                    className={`${
+                      dark ? "text-gray-300" : "text-gray-500"
+                    } font-satoshiRg text-lg mx-2`}
+                  >
+                    GPA
+                  </p>
+                  <p
+                    className={`${
+                      dark ? "text-gray-300" : "text-gray-500"
+                    } font-satoshiRg text-lg mx-2`}
+                  >
+                    <strong className="text-primary-500">3.05 </strong>/ 4.0
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="">
+              <Headings name="Years of experience" reverse={true} />
+              <div
+                className={`mt-2 ${
+                  dark ? "text-white" : "text-black"
+                } font-satoshiB text-right text-3xl mx-2`}
+              >
+                <Counter value={1} />+ years
+              </div>
+            </div>
+            <div className="">
+              <div
+                className={`flex flex-row items-center justify-between mx-2`}
+              >
+                <h2
+                  className={`font-satoshiMd ${
+                    dark ? "text-gray-300" : "text-gray-500"
+                  }  text-3xl`}
+                >
+                  Experience
+                </h2>
+                <div
+                  onMouseEnter={ExperienceEnter}
+                  onMouseLeave={Leave}
+                  className="w-14 h-14 border border-black flex flex-row items-center justify-center rounded-full bg-[#FFC633]"
+                >
+                  <AiOutlineArrowRight className="w-8 h-8 -rotate-45" />
+                </div>
+              </div>
+              <ExperienceBrief
+                position={"Data Scientist"}
+                company={"Funavry Technologies · Full-time"}
+                date={"06/2023 - Current"}
+              />
+              <ExperienceBrief
+                position={"Data Scientist"}
+                company={"Musketeers Tech · Internship"}
+                date={"Sep 2022 - Nov 2022 · 3 mos"}
+              />
+              <ExperienceBrief
+                position={"Machine Learning Engineer"}
+                company={"AIM Lab · Internship"}
+                date={"Jul 2021 - Aug 2021 · 2 mos"}
+              />
+            </div>
+            <div className={""}>
+              <Headings name="Location" reverse={true} />
+              <p
+                className={`mt-2 ${
+                  dark ? "text-white" : "text-black"
+                } font-satoshiRg text-lg mx-2`}
+              >
+                Born and raised in Pakistan.
+              </p>
+              <div className="main w-40 h-40 float-right">
+                <img src={faisal} width="65" height="65" />
+                <svg id="rotatingText" width="150" height="150">
+                  <defs>
+                    <path
+                      id="circle"
+                      d="M 75, 75
+                 m -56.25, 0
+                 a 56.25, 56.25 0 1, 0 112.5, 0
+                 a 56.25, 56.25 0 1, 0 -112.5, 0
+                 "
+                    ></path>
+                  </defs>
+                  <text width="400">
+                    <textPath
+                      alignmentBaseline="top"
+                      xlinkHref="#circle"
+                      className="text font-satoshiRg"
+                    >
+                      · Based in Pakistan · Based in Pakistan
+                    </textPath>
+                  </text>
+                </svg>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className={`${showSkills ? "block":"hidden"} shadow-sm p-2 m-1 bg-white rounded-2xl text-[#FFC145]  space-y-1 cursor-default`}>
-      <p>Team player</p>
-      <p>Autodidactic</p>
-      <p>Adaptable</p>
-      <p>Creative</p>
-      </div>
-    </motion.div>
-</div>
-<div className="div6 z-10 place-self-start gap-2">
-<div className={`rounded-full flex flex-col items-center p-5 mx-5 justify-center  bg-like text-white`}>
-    <img src={heart} className='w-9'/>
-    <p className='educate-heading text-xl font-generalSansMd'>Things I like to do</p>
-  </div>
-</div>
-<div className="div7 z-10 gap-2 place-self-start">
-  <div className='subdiv1'>
-  <img src={codesticker} className='w-20 rounded-full'/>
-  </div>
-  <div className='subdiv2'>
-  <motion.div whileHover={{scale:1.05}} className='rounded-full w-20 h-20 bg-[#454545]'><img src={guitar} className='w-20 rounded-full'/></motion.div>
-  
-  </div>
-  <div className='subdiv3'>
-  <div className='rounded-full bg-[#0B2521] w-20 h-20 flex flex-row items-center justify-center'>
-    <img src={spotify} className='w-12 rounded-full'/></div>
-   </div>
-   <div className='subdiv4 place-self-center'>
-   <motion.div whileHover={{scale:1.05}} className=''><img src={game} className='rounded-full'/></motion.div>
-  </div>
-  <div className='subdiv5 place-self-center'>
-  <img src={brain} className=' rounded-full '/> 
     </div>
-  </div>
-  </div>
-    </div>
-    </div>
-  )
+  );
 }
