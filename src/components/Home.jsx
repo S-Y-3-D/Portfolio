1f9e9002ef8acafdde118b74e3045df4739a8934
Syed Hamza <syedhamzatahir1001@gmail.com>
2023-06-11 04:03:41 -0700

    3d elements z index changed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,20 +3,35 @@ import { Parallax, ParallaxLayer } from '@react-spring/parallax'
 import { useSelector, useDispatch } from 'react-redux'
 import Scene from '../components/Spline/Scene'
 import DarkScene from '../components/Spline/DarkScene';
+import { Player, Controls } from '@lottiefiles/react-lottie-player';
 
 export default function Home() {
   
   const dark = useSelector((state) => state.switch.value)
   return (
-    <div className={`border h-screen ${dark ? "border-black" : "border-white"} `}>
-    {dark ? <DarkScene /> :<Scene/>}
-    <div className={`text-center mx-auto my-52 w-3/4 ${dark && "text-primary-800"}`}>
-        <h2 className='font-chillaxMd text-5xl'>
+    <div className={`bg-transparent h-screen`}>
+    <div className={`text-center  flex flex-col h-full items-center justify-center ${dark && "text-white"}`}> 
+      {dark ? <DarkScene /> :<Scene/>}
+        <h2 className='font-outfitMd text-7xl'>
         I’m <strong className='text-primary-500'>Syed</strong>. I will create your dream web and mobile application.
         </h2>
-        <h3 className='font-generalSansRg text-lg w-4/5 mx-auto mt-5'>
-        Elevate Your Business with Expertly Designed Web and Mobile Applications that Drive Results and Captivate Your Audience.
-        </h3>
+        <div className='flex flex-row items-center justify-center mt-10'>
+        <h2 className={`${dark ? "text-gray-300" : "text-gray-500" }  font-satoshiRg text-2xl `}>Embrace your upcoming project with&#160;</h2>
+        <div className='active font-outfitMd'>
+          <ul>
+            <li> Reliability</li>
+            <li> Usability</li>
+            <li> Quality</li>
+          </ul>
+        </div>
+        <div></div>
+        </div>
+        <div className={`flex gap-3 items-center justify-center w-[40%]   mt-10`}>
+         <button className=' font-satoshiMd text-white text-xl background-connect w-1/2 rounded-lg p-3'>Let's Connect
+         </button>
+         <button className={`font-satoshiMd text-xl  bg-gray-100 text-black w-1/2 rounded-lg  p-3`}>Know More </button>
+        </div>
+        <h2 className={`${dark ? "text-gray-300" : "text-gray-500" } mt-5  font-satoshiRg text-lg `}>Connect to discuss the next big project</h2>
         <div>
         </div>
       {/* <div className="flex items-center justify-center">
