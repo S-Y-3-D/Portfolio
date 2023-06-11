edc4d7cbb17c732dc51a9d983e191ed00b3e3581
Syed Hamza <syedhamzatahir1001@gmail.com>
2023-06-11 04:05:44 -0700

    Fonts changed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,15 +10,14 @@ export default function Navbar() {
 
   return (
     <div  className='navbar flex items-center justify-between'>
-        <h3 className={`font-chillaxSemi text-3xl cursor-pointer ${dark && "text-primary-800"}`}>Syed.</h3>
-        <div className={`flex gap-10 font-chillaxMd ${dark && "text-primary-800"}`}>
+        <h3 className={`font-outfitB text-3xl cursor-pointer ${dark && "text-white"}`}>Syed.</h3>
+        <div className={`flex gap-10 font-satoshiMd ${dark && "text-white"}`}>
         <h4 className='cursor-pointer'>About</h4>
         <h4 className='cursor-pointer'>Projects</h4>
         <h4 className='cursor-pointer'>Experience</h4>
         </div>
         <div className='flex flex-row items-center gap-6'>
         {dark ? <BsFillSunFill onClick={() => dispatch(switchColor())} color='#FFFFE3'  className='cursor-pointer'  size={24}/> : <BsFillMoonFill onClick={() => dispatch(switchColor())} className='cursor-pointer'  size={24}/> }
-        <button className='font-chillaxMd text-white bg-primary-500 p-3 rounded-full'>Let's Talk</button>
         </div>
         
     </div>
