cff5301ccbacc3e1dda0f5145b03d296b618355f
Syed Hamza <syedhamzatahir1001@gmail.com>
2023-06-11 04:05:29 -0700

    Z index changed

--- src/components/Spline/Scene.jsx
+++ src/components/Spline/Scene.jsx
@@ -3,10 +3,11 @@ const Spline = React.lazy(() => import('@splinetool/react-spline'));
 
 export default function Scene() { 
   return (
-    <div className='absolute top-44  right-0  h-3/4 w-2/4'>
+    <div className='absolute -z-10 top-0 w-full h-full right-0 '>
           <Suspense fallback={<div>Loading...</div>}>
-         <Spline scene="https://prod.spline.design/sEqwZRIDWJwUjstQ/scene.splinecode" />
+         <Spline className='' scene="https://prod.spline.design/sEqwZRIDWJwUjstQ/scene.splinecode" />
          </Suspense>
     </div>
-  )
+  ) 
 }
+ 
