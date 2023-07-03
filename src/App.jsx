9105fe848359051529fe24820d1572c47181de81
Syed Hamza <syedhamzatahir1001@gmail.com>
2023-07-03 13:36:45 -0700

    Added components

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,65 @@
-import './App.css'
-import Navbar from './components/Navbar';
-import Home from './components/Home';
-import { useSelector} from 'react-redux'
-import Web from './components/Projects/Web';
-import TechScene from './components/Spline/TechScene';
-import React, { useEffect } from 'react';
-import About from './components/About';
-
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import { useSelector } from "react-redux";
+import Web from "./components/Projects/Web";
+import TechScene from "./components/Spline/TechScene";
+import React, { useEffect } from "react";
+import About from "./components/About";
+import { Routes, Route, useLocation} from "react-router-dom";
+import Education from "./components/Routes/Education";
+import { AnimatePresence } from "framer-motion";
+import Experience from './components/Routes/Experience';
+import Footer from './components/Footer';
 function App() {
-
-  const dark = useSelector((state) => state.switch.value)
+  const dark = useSelector((state) => state.switch.value);
+  const location = useLocation()
 
   useEffect(() => {
-    const onPageLoad = () => {
-    };
+    const onPageLoad = () => {};
 
     // Check if the page has already loaded
-    if (document.readyState === 'complete') {
+    if (document.readyState === "complete") {
       onPageLoad();
     } else {
-      window.addEventListener('load', onPageLoad);
+      window.addEventListener("load", onPageLoad);
       // Remove the event listener when component unmounts
-      return () => window.removeEventListener('load', onPageLoad);
+      return () => window.removeEventListener("load", onPageLoad);
     }
   }, []);
 
-  document.body.style.backgroundColor = dark ? "black" : "white"
+  document.body.style.backgroundColor = dark ? "#1E1E20" : "white";
+
+  function Main() {
+    return (
+    <><div className="content">
+      <Home />
+      <About />
+      {/* <Web /> */}
+    </div>
+</>
+  )
+  }
   return (
     <>
-    <div className='content'>
-
-    <Navbar/>
-    <Home/>
-    <About/>
-    </div>
-    </>
-    // <>
-    // <div className={`${dark && "bg-black"} -z-20 h-full`}>
-    //  <div className='content'>
-    //  <Navbar/>
-    //  <Home/>
-    //  {/* <About/>
-    //   <Web/> */}
+      <div className="content">
+          <Navbar />
+        </div>
+      <AnimatePresence mode="wait">
+        <Routes  location={location} key={location.pathname}>
+          <Route path="/" element={<Main/>}>
+            
+          </Route>
+          <Route path="/education" element={<Education/>}>
+          </Route>
+          <Route path="/experience" element={<Experience/>}>
+          </Route>
+        </Routes>
       
-    //  </div>
-    //  {/* <TechScene/> */}
-    // </div>
-    // </>
-    )
+      </AnimatePresence>
+      <Footer/>
+    </>
+  );
 }
 
-export default App
+export default App;
